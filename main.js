abc3a17fc2d97a4d00c4ebbec453d05bf04ddb4d
nejiko96 <nejiko2006@gmail.com>
2022-04-10 17:14:39 +0900

    WordleHelper::serach() implementation

--- main.js
+++ main.js
@@ -4,14 +4,21 @@ import WordleHelper from './wordle-helper'
 import './style.css'
 
 const run = () => {
-  const tried = $('#tried').val()
-  const got = $('#got').val()
-  const allowed = $('#allowed').val()
-  const wordle = new WordleHelper(tried, got, allowed)
-  const result = wordle.search()
-  const sg = wordle.suggest()
-  $('#result').val(result.join('\n'))
-  $('#suggestion').val(sg.map(e => `${e[0]}(${e.slice(1, 4).join('-')})`).join('\n'))
+  try {
+    const tried = $('#tried').val()
+    const got = $('#got').val()
+    const allowed = $('#allowed').val()
+    const wordle = new WordleHelper(tried, got, allowed)
+    const results = wordle.search()
+    const resultsDisp = [results.slice(0, 10).join(' ')]
+    if (results.length > 10) resultsDisp.push('...')
+    resultsDisp.push(` (total ${results.length} words)`)
+    $('#result').val(resultsDisp.join('\n'))
+    const sgst = wordle.suggest()
+    $('#suggestion').val(sgst.map(e => `${e[0]}(${e.slice(1, 4).join('-')})`).join('\n'))
+  } catch (error) {
+    $('#result').val(error)
+  }
 }
 
 $('#frm').on('submit', () => {
@@ -23,8 +30,3 @@ $('#tried,#got,#allowed').on('keydown', () => {
   $('#result').val('')
   $('#suggestion').val('')
 })
-
-// document.querySelector('#app').innerHTML = `
-//   <h1>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `
--- wordle-helper.js
+++ wordle-helper.js
@@ -1,14 +1,47 @@
-class WordleHelper {
-  constructor(tried, got, allowed) {
+import wordsContent from './words.txt?raw'
 
+class WordleHelper {
+  static __words = wordsContent.split('\n')
+
+  constructor(tried, got, allowed) {
+    this._tried = tried.toUpperCase().replace(/[^A-Z]/g, '')
+    this._got = got.toUpperCase().replace(/[^A-Z]/g, '')
+    this._allowed = allowed.toUpperCase().replace(/\[\^?/g, '[^')
+  }
+
+  get words() {
+    return WordleHelper.__words
+  }
+
+  get excludePat() {
+    if (this._excludePat === undefined) {
+      const gotRe = new RegExp(`[_${this._got}]`, 'g')
+      const othersArrRaw = [...this._tried.replace(gotRe, '')]
+      const othersArrUniq = [...new Set(othersArrRaw)]
+      const othersStr = othersArrUniq.join('')
+      this._excludePat = new RegExp(`[_${othersStr}]`)
+    }
+    return this._excludePat
+  }
+
+  get includePat() {
+    if (this._includePat === undefined) {
+      const contains = [...this._got].map(c => `(?=.*${c})`).join('')
+      this._includePat = new RegExp(`^${contains}${this._allowed}$`)
+    }
+    return this._includePat
   }
 
   search() {
-    return [
-      'BABKA',
-      'BABUL',
-      'BACCA'
-    ]
+    // const dbg = [
+    //   this._tried,
+    //   this._got,
+    //   this._allowed,
+    //   this.exclude_pat,
+    //   this.include_pat
+    // ]
+    const ret = this.words.filter(w => !w.match(this.excludePat) && w.match(this.includePat))
+    return ret
   }
 
   suggest() {
