beaf297195c2e02d74c6c8aebf095c11f8d920e4
nejiko96 <nejiko2006@gmail.com>
2022-04-10 12:35:57 +0900

    WordleHelperの枠まで

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
+import $ from 'jquery'
+import WordleHelper from './wordle-helper'
+
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const run = () => {
+  const tried = $('#tried').val()
+  const got = $('#got').val()
+  const allowed = $('#allowed').val()
+  const wordle = new WordleHelper(tried, got, allowed)
+  const result = wordle.search()
+  const sg = wordle.suggest()
+  $('#result').val(result.join('\n'))
+  $('#suggestion').val(sg.map(e => `${e[0]}(${e.slice(1, 4).join('-')})`).join('\n'))
+}
+
+$('#frm').on('submit', () => {
+  run()
+  return false
+})
+
+$('#tried,#got,#allowed').on('keydown', () => {
+  $('#result').val('')
+  $('#suggestion').val('')
+})
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
---
+++ wordle-helper.js
@@ -0,0 +1,23 @@
+class WordleHelper {
+  constructor(tried, got, allowed) {
+
+  }
+
+  search() {
+    return [
+      'BABKA',
+      'BABUL',
+      'BACCA'
+    ]
+  }
+
+  suggest() {
+    return [
+      ['AEROS', 27867, 8205, 5],
+      ['SOARE', 27867, 8205, 5],
+      ['AROSE', 82767, 4699, 5]
+    ]
+  }
+}
+
+export default WordleHelper
